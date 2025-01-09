6b2eb936ea9c11a1eb108814271fdf51999c5e69
licheng <cheng.li29@pactera.com>
2025-01-09 14:54:52 +0800

    hello world

--- src/main/java/com/github/config/UserDetailsServiceImpl.java
+++ src/main/java/com/github/config/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         //以下过程主要模拟从数据库中获取用户信息
         if ("admin".equals(username)) {
             return new SysUser("admin", "123456", null);
-        } else if ("zhangsan".equals(username)) {
+        } else if ("zhangsan".equals(username )) {
             return new SysUser("zhangsan", "123456", null);
         } else {
             throw new UsernameNotFoundException("用户不存在");
